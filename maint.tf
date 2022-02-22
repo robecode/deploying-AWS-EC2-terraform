b01ad95d0cd103412fde35c04da0088e15549d1b
Roberto S <ro11x@icloud.com>
2022-02-22 15:12:14 -0500

    Create maint.tf

---
+++ maint.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "EC1" {
+  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
+
+   tags = {
+    Name = "Ubuntu1"
+  }
+}
+
+resource "aws_instance" "EC2" {
+  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
+
+   tags = {
+    Name = "Ubuntu2"
+  }
+ }
+
